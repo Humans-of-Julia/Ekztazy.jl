33d3d2ba1930ccbf74b920bc3fe5d9f4236898bf
Kyando <amehikoji@gmail.com>
2021-10-08 00:41:26 -0400

    Remove useless unittest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,3 @@
 using Dizkord
 using Test 
 
-client = Client(
-    "ODMwMjA4MDEyNjY4NzY0MjUw.YHDVdg.98_US-9CVfQImroXOF4DowNDM6M", 
-    830208012668764250,
-    intents(GUILDS, GUILD_MESSAGES)
-)
-
-on_message!(client) do (ctx) 
-    print("Received Message: $(ctx.content)")
-    return nothing
-end
-
-open(client)
