c0943f52e3f8f333bca5822c953f5985eb8bea30
Kyando2 <amehikoji@gmail.com>
2021-10-08 21:47:15 -0400

    ...

--- src/types/types.jl
+++ src/types/types.jl
@@ -95,7 +95,7 @@ macro constructors(T)
 
     quote
         function $(esc(T))(; kwargs...) 
-            @debug "Got some stuff" kw=$kwargs ar=$args 
+            @debug "Got some stuff" kw=$args
             $(esc(T))($(args...))
         end
         $(esc(T))(d::Dict{Symbol, Any}) = $(esc(T))(; d...)
