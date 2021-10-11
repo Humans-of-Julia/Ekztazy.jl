bb43628b782386e06fc9568a381651af963298b2
Kyando2 <amehikoji@gmail.com>
2021-10-11 00:04:40 -0400

    ...

--- src/client/client.jl
+++ src/client/client.jl
@@ -151,7 +151,7 @@ end
 
 Client(token::String, appid::Int, intents::Int, args...) = Client(token, UInt(appid), intents, args...)
 
-magic_add!(d::Dict{U, Vector{<:T}}, k::U, v::T) where T where U = haskey(d, k) ? push!(d[k], v) : d[k] = [v]
+add!(d::Dict{U, Vector{<:T}}, k::U, v::T) where T where U = haskey(d, k) ? push!(d[k], v) : d[k] = [v]
 
 add_handler!(c::Client, handler::AbstractHandler) = add!(c.handlers, Symbol(typeof(handler)), handler)
 add_handler!(c::Client, args...) = map(h -> add_handler!(c, h), args)
