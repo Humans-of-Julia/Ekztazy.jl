c9182ee65e075816453ee9d54579676415c5f7ff
Kyando2 <amehikoji@gmail.com>
2021-10-11 00:22:39 -0400

    ...

--- test/fulltest.jl
+++ test/fulltest.jl
@@ -16,10 +16,6 @@ command!(client, 776251117616234506, "boom", "Go boom!") do (ctx)
     Dizkord.reply(client, ctx, content="<@$(ctx.int.member.user.id)> blew up!")
 end
 
-command!(client, 776251117616234506, "bam", "Go bam!") do (ctx) 
-    Dizkord.reply(client, ctx, content="<@$(ctx.int.member.user.id)> slapped themselves!")
-end
-
 command!(client, 776251117616234506, "quit", "Ends the bot process!") do (ctx) 
     Dizkord.reply(client, ctx, content="Shutting down the bot")
     close(client)
