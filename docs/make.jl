084ce1eef1bba8afbd324c475c191ebd55f54dbb
Kyando2 <amehikoji@gmail.com>
2021-10-11 13:17:14 -0400

    ...

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,9 @@ using Documenter, Dizkord
 makedocs(;
     modules=[Dizkord],
     format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md"
+    ]
     repo="https://github.com/Kyando2/Dizkord.jl/blob/{commit}{path}#L{line}",
     sitename="Dizkord.jl",
     authors="Xh4H <sindur.esl@gmail.com>, christopher-dG <chrisadegraaf@gmail.com> Kyando <izawa.iori.tan@gmail.com>",
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,4 @@
-```@autodocs
-Modules = [Dizkord]
+## Index
+
+```@index
 ```
